5c93a2fc31f92a877caaf9f99844a028341cbeb4
kenshin985 <kenshin985@gmail.com>
2019-03-23 12:31:16 +0100

    added webflux to dependencies, changed testRestTemplate to webTestClient

--- src/test/java/com/piotrek/gamecalendar/game/GameControllerTest.java
+++ src/test/java/com/piotrek/gamecalendar/game/GameControllerTest.java
@@ -1,5 +1,6 @@
 package com.piotrek.gamecalendar.game;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.piotrek.gamecalendar.AbstractIntegrationTest;
 import com.piotrek.gamecalendar.exceptions.ErrorResponse;
@@ -8,19 +9,12 @@ import com.piotrek.gamecalendar.gameReleaseDate.GameReleaseDateRepository;
 import com.piotrek.gamecalendar.gamingPlatform.GamingPlatform;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import javax.annotation.Resource;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
-
-import static org.assertj.core.api.BDDAssertions.then;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class GameControllerTest extends AbstractIntegrationTest {
 
@@ -31,9 +25,10 @@ class GameControllerTest extends AbstractIntegrationTest {
     private GameReleaseDateRepository gameReleaseDateRepository;
 
     @Resource
-    private TestRestTemplate testRestTemplate;
+    private WebTestClient webTestClient;
 
-    private ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
+    @Resource
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void beforeEach() {
@@ -42,58 +37,58 @@ class GameControllerTest extends AbstractIntegrationTest {
     }
 
     @Test
-    void shouldReturnGameByGivenId() {
+    void shouldReturnGameByGivenId() throws JsonProcessingException {
         // given
         var expectedGame = gameRepository.save(Game.builder()
                 .id(1L)
                 .name("The Witcher® 3: Wild Hunt")
                 .releaseDates(Set.of()).build());
-        final var URL = "/games/" + expectedGame.getId();
 
         // when
-        ResponseEntity<Game> response = testRestTemplate.getForEntity(URL, Game.class);
+        var exchange = webTestClient.get()
+                .uri("/games/" + expectedGame.getId())
+                .exchange();
 
         // then
-        assertAll(
-                () -> then(response.getStatusCode()).isEqualTo(HttpStatus.OK),
-                () -> then(response.getBody()).isEqualTo(expectedGame)
-        );
+        exchange
+                .expectStatus().is2xxSuccessful()
+                .expectBody().json(objectMapper.writeValueAsString(expectedGame));
     }
 
     @Test
-    void shouldThrowNotFoundExceptionWhenNotFoundGame() {
+    void shouldThrowNotFoundExceptionWhenNotFoundGame() throws JsonProcessingException {
         // given
         var expectedResponse = new ErrorResponse(404, "Not found game with id 3");
 
         // when
-        var response = testRestTemplate.getForEntity("/games/3", ErrorResponse.class);
+        var exchange = webTestClient.get()
+                .uri("/games/3")
+                .exchange();
 
         // then
-        assertAll(
-                () -> then(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND),
-                () -> then(response.getBody()).isEqualTo(expectedResponse)
-        );
+        exchange
+                .expectStatus().is4xxClientError()
+                .expectBody().json(objectMapper.writeValueAsString(expectedResponse));
     }
 
     @Test
-    void shouldReturnEmptySetWhenNoPremieresInThisMonth() {
+    void shouldReturnEmptySetWhenNoPremieresInThisMonth() throws JsonProcessingException {
         // given
         var expectedSet = new HashSet<Game>();
 
         // when
-        var response = testRestTemplate.getForEntity("/games/this-month-premieres", Set.class);
+        var exchange = webTestClient.get()
+                .uri("/games/this-month-premieres")
+                .exchange();
 
         // then
-        assertNotNull(response.getBody());
-        assertAll(
-                () -> then(response.getStatusCode()).isEqualTo(HttpStatus.OK),
-                () -> then(response.getBody()).isEqualTo(expectedSet),
-                () -> then(response.getBody().size()).isEqualTo(0)
-        );
+        exchange
+                .expectStatus().is2xxSuccessful()
+                .expectBody().json(objectMapper.writeValueAsString(expectedSet));
     }
 
     @Test
-    void shouldReturnOneElementInSetWhenOneGameHasPremiereInThisMonth() {
+    void shouldReturnOneElementInSetWhenOneGameHasPremiereInThisMonth() throws JsonProcessingException {
         // given
         var gameReleaseDate = gameReleaseDateRepository.save(GameReleaseDate.builder()
                 .gamingPlatform(GamingPlatform.builder().name("PC").build())
@@ -108,14 +103,13 @@ class GameControllerTest extends AbstractIntegrationTest {
         expectedSet.add(onlyGame);
 
         // when
-        var response = testRestTemplate.getForEntity("/games/this-month-premieres", Set.class);
+        var exchange = webTestClient.get()
+                .uri("/games/this-month-premieres")
+                .exchange();
 
         // then
-        assertNotNull(response.getBody());
-        assertAll(
-                () -> then(response.getStatusCode()).isEqualTo(HttpStatus.OK),
-                () -> then(response.getBody()).isEqualTo(objectMapper.writeValueAsString(expectedSet)),
-                () -> then(response.getBody().size()).isEqualTo(expectedSet.size())
-        );
+        exchange
+                .expectStatus().is2xxSuccessful()
+                .expectBody().json(objectMapper.writeValueAsString(expectedSet));
     }
 }
